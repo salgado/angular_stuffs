ef2e5ab724caf05558c6cf9f6d694201604f82eb
Alex Salgado <alexsalgado1@gmail.com>
2016-10-03 13:55:42 -0300

    create mod folder

--- assign2/app.js
+++ assign2/app.js
@@ -155,18 +155,31 @@ angular.module('confusionApp', [])
         .controller('DishCommentController', ['$scope', function($scope) {
             
             //Step 1: Create a JavaScript object to hold the comment from the form
+            $scope.comment = {
+                              name: "",
+                              rating: 5,
+                              comment: "",
+                              date: ""
+                            };
             
             $scope.submitComment = function () {
                 
                 //Step 2: This is how you record the date
-                "The date property of your JavaScript object holding the comment" = new Date().toISOString();
+                $scope.comment.date = new Date().toISOString();
                 
                 // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push($scope.comment);
                 
                 //Step 4: reset your form to pristine
+                //$scope.commentForm.$setPristine();
                 
                 //Step 5: reset your JavaScript object that holds your comment
+                $scope.comment = {
+                              name: "",
+                              rating: 5,
+                              comment: "",
+                              date: ""
+                            };
             }
         }])
 
